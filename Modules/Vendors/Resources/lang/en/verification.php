6b83ca0b2dccf7b30b48242eaea3d657635f42ac
Ehab Barakat <dev.ehab.barakat@gmail.com>
2025-11-14 17:48:29 +0200

    translation

--- Modules/Vendors/Resources/lang/en/verification.php
+++ Modules/Vendors/Resources/lang/en/verification.php
@@ -5,6 +5,8 @@ return [
     'sent' => 'The verification code has been sent successfully.',
     'verified' => 'Your phone number already verified.',
     'is_verified' => 'Your Account has been verified successfully',
+    'phone_not_verified' => 'Phone is not verified, we have sent you an sms with code',
+    'email_not_verified' => 'Email is not verified, we have sent you an email with code',
     'attributes' => [
         'user_id' => 'User',
         'phone' => 'Phone',
